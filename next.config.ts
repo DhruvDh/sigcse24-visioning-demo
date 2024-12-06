96c55c8f3235c3271ee4ae1ae41abb0f7bd8d94e
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2024-12-06 13:11:36 -0500

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   reactStrictMode: true,
-  swcMinify: true,
+  distDir: "out",
 };
 
 export default nextConfig;
